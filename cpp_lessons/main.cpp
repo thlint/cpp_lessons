fa03f4e292f06096a272000d062b561620372855
File404Found1 <File404Found1@yandex.ru>
2019-01-24 15:22:50 +0300

    добавляем русский язык

--- cpp_lessons/main.cpp
+++ cpp_lessons/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main()
 {
+//	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	cout << "Привет!" << endl;
 	system("pause");
 	return 0;
