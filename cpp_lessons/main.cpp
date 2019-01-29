f9205fecc9d825b62a610b66c7c67969bc01b981
File404Found1 <File404Found1@yandex.ru>
2019-01-29 10:53:54 +0300

    урок 5 массивы, русский ввод

--- cpp_lessons/main.cpp
+++ cpp_lessons/main.cpp
@@ -1,4 +1,4 @@
-﻿// урок 4 массивы.
+﻿// урок 5 массивы, русский ввод
 // уроки по cpp https://ravesli.com/uroki-cpp/   https://code-live.ru/tag/cpp-manual/
 
 
@@ -8,7 +8,10 @@
 using namespace std;
 int main()
 {
-	SetConsoleOutputCP(1251);
+    SetConsoleOutputCP(1251); //Вывод
+	SetConsoleCP(1251);		  // Ввод
+	//setlocale(LC_ALL, "Russian");
+
 	string students[3] = { "Иванов", "Петров", "Сидоров" };
 	// вывод 1 - если объявить переменную прямо в теле цикла for, то когда заканчивается тело цикла, эта переменная исчезает
 	for (int i = 0; i < 3; i++) {
@@ -20,11 +23,14 @@ int main()
 	for (int i = 0; i < 3; i++) {
 		cout << "for " << students[i] << endl;
 	}
-		cout << "переменная i объявлена внутри цсловия цикла for и не влияет на внешнюю переменную i " << i << endl;
-	for (i = 0; i < 3; i++) {
-			cout << "for " << students[i] << endl;
+		cout << "переменная i объявлена внутри цсловия цикла for и не влияет на внешнюю переменную i (выведет 30) " << i << endl;
+		string students1[3];
+		for (i = 0; i < 3; i++) {
+			cout << endl << "напишите фамилию студента";
+			cin >> students1[i];
+			cout << "for " << "i=" << i << " " <<students1[i] << endl;
 		}
-	cout << "цикл for использует внешнюю переменну i, которая объявлена заранее " << i << endl;
+	cout << "цикл for использует внешнюю переменну i, которая объявлена заранее (выведет не 2, а 3, т.к. i++ выполняется после проверки i < 3 ) " << i << endl;
 	
 	while (i < 3) {
 		cout << "while " << students[i] << endl;
