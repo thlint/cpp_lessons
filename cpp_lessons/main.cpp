3ea4cac9ac5b803e51c04b018bc4d69ed2d77cb9
File404Found1 <File404Found1@yandex.ru>
2019-01-29 12:41:16 +0300

    Урок 6 функции и длина строки

--- cpp_lessons/main.cpp
+++ cpp_lessons/main.cpp
@@ -1,4 +1,4 @@
-﻿// урок 5 массивы, русский ввод
+﻿// урок 6 функции
 // уроки по cpp https://ravesli.com/uroki-cpp/   https://code-live.ru/tag/cpp-manual/
 
 
@@ -6,6 +6,20 @@
 #include <windows.h>
 #include <string>
 using namespace std;
+void dim3_output(string dim_for_output[3]) { // void - значит функция как процедура - не выводит значения (нет типа)
+		for (int i = 0; i < 3; i++) {
+			cout << "функция выводит очередное значение " << dim_for_output[i] << " длина значения dim_for_output[0].size() = " << dim_for_output[i].size()  << endl;
+	}
+}
+
+string my_string_concat(string my_str1, string my_str2, int i) {
+	string my_output_str = "всегда одно и то же";
+	cout << "Функция выводит " << my_output_str << endl;
+	string i_string;
+	my_output_str = my_str1 + my_str2;
+	return my_output_str;
+}
+
 int main()
 {
     SetConsoleOutputCP(1251); //Вывод
@@ -28,9 +42,18 @@ int main()
 		for (i = 0; i < 3; i++) {
 			cout << endl << "напишите фамилию студента";
 			cin >> students1[i];
-			cout << "for " << "i=" << i << " " <<students1[i] << endl;
+			cout << "for " << "i=" << i << " " << students1[i] << endl;
 		}
-	cout << "цикл for использует внешнюю переменну i, которая объявлена заранее (выведет не 2, а 3, т.к. i++ выполняется после проверки i < 3 ) " << i << endl;
+	
+		cout << "Используем функцию для вывода массива ------------- i=" << i << endl;
+		dim3_output(students1);
+		cout << "Использовали функцию для вывода массива -------------i= " << i << endl;
+		
+		cout << "Используем функцию ------------ " << endl;
+		cout << "Функция вернула " << my_string_concat(" Строка 1 ", " Строка 2 ", 2) << endl;
+		cout << "Использовали функцию ----------- " << endl;
+
+		cout << "цикл for использует внешнюю переменну i, которая объявлена заранее (выведет не 2, а 3, т.к. i++ выполняется после проверки i < 3 ) " << i << endl;
 	
 	while (i < 3) {
 		cout << "while " << students[i] << endl;
