ab91f05cd1f86b808661cc7c5f1280439c80922f
File404Found1 <File404Found1@yandex.ru>
2019-01-31 17:00:09 +0300

    удаляем строки из emails.txt

--- cpp_lessons/main.cpp
+++ cpp_lessons/main.cpp
@@ -1,42 +1,37 @@
-﻿// урок 7 указатели урок 8 динамические массивы урок 8 параметры командной строки
+﻿// подсчет строк во входном файле.
 // уроки по cpp https://ravesli.com/uroki-cpp/   https://code-live.ru/tag/cpp-manual/
 #include <iostream>
 #include <windows.h>
+#include <fstream>
+#include <string>
 using namespace std;
-int main(int argcount, char *argv[]) //количество аргументов-параметров командной строки, указатель на массив с аргументами
+int main(int argcount, char *argv[]) //количество аргументов-параметров командной строки, указатель на массив argv[] с аргументами argv[0] - имя запускаемой программы
 {
-    SetConsoleOutputCP(1251); //Вывод
+    SetConsoleOutputCP(1251); //Вывод русских символов
 	SetConsoleCP(1251);		  // Ввод
-	
-		for (int i = 0; i < argcount; i++) cout << "Argument " << i << " : " << argv[i] << endl; 	// Выводим список аргументов в цикле
-	
-	int a; //объявление статической переменной
-	int b = 5; //инициализация статической переменной
-	int *c = new int; //объявление указалется для переменной типа int
-	int *d = new int(5); //инициализация указателя для переменной типа int
-	int* ptr_a = &a; // указатель ptr_a "смотрит" на a
-	*c = 10;
-	cout << "*c = " << *c << endl;
-	cout << "*d = " << *d << endl;
-	a = 15;
-	cout << "*ptr_a = " << *ptr_a << endl; //выводится переменная a
-	cout << "ptr_a = " << ptr_a << endl; //выводится адрес переменной a
-	float *e = new float(3.14);
-	a = 3 + 5 + *c + b;
-	cout << "*ptr_a = " << *ptr_a << endl; //выводится переменная a
-	cout << "*float *e = " << *e - 0.01 << endl; //выводится резултат работы с переменной e типа float
-	delete e; // освобождает место
-//		cout << "*float *e = " << *e - 0.01 << endl; //вызывает останов программы?
-
-// динамические массивы	
-	int num; // Размер массива
-	num = 11;
-	int *p_darr = new int[num]; //выделение памяти для массива
-	for (int i = 0; i < num; i++) {
-		if (i == 0) p_darr[i] = 1;
-		else p_darr[i] = p_darr[i-1] + i ;
-		cout << "Значение " << i << " -го элемента равно " << p_darr[i] << endl;
+	if (argcount < 3) {
+		cout << "Укажите файл со списком разрешенных адресов и файл со списком адресов для блокировки в параметрах командной строки" << endl;
+		cout << "например remove_addr.exe emails.txt emails_to_block.txt" << endl;
+		cout << "на основе emails.txt будет создан новый сокращенный файл - emails2.txt " << endl << "в emails2.txt будут удаленны адреса по списку из emails_to_block.txt" << endl;
+		return 0;
+		system("pause");
 	}
+		int count_line_emails = 0; // инициируем кол-во строк в файле
+		string line_from_file; // для очередной строки из файла
+		ifstream input_file_with_emails; 
+		input_file_with_emails.open(argv[1]);// открываем файл (имя из первого аргумета) для чтения 
+		while (getline(input_file_with_emails, line_from_file)) count_line_emails++; // в файле всего count_line_emails строк
+		input_file_with_emails.close();  //закрыли файл
+		string *all_emails = new string[count_line_emails]; // динамические массив, обращение к элементу all_lines[i]
+		input_file_with_emails.open(argv[1]); //повтороное открытие файла
+		for (int i = 0; i < count_line_emails; i++) getline(input_file_with_emails, all_emails[i]); // заполняем динамический массив строк  *all_emails
+		input_file_with_emails.close();  //закрыли файл
+					
+		for (int i = 0; i < count_line_emails; i++) cout << "очередная строка из файла с адресами " << all_emails[i] << endl;  // проверка входного файла
+
+		ifstream input_file_with_blocks;
+		input_file_with_blocks.open(argv[2]);
+
 	system("pause");
 	return 0;
 }
---
+++ cpp_lessons/main7-9.cpp
@@ -0,0 +1,42 @@
+﻿// урок 7 указатели урок 8 динамические массивы урок 9 параметры командной строки
+// уроки по cpp https://ravesli.com/uroki-cpp/   https://code-live.ru/tag/cpp-manual/
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main(int argcount, char *argv[]) //количество аргументов-параметров командной строки, указатель на массив с аргументами
+{
+    SetConsoleOutputCP(1251); //Вывод
+	SetConsoleCP(1251);		  // Ввод
+	
+		for (int i = 0; i < argcount; i++) cout << "Argument " << i << " : " << argv[i] << endl; 	// Выводим список аргументов в цикле
+	
+	int a; //объявление статической переменной
+	int b = 5; //инициализация статической переменной
+	int *c = new int; //объявление указалется для переменной типа int
+	int *d = new int(5); //инициализация указателя для переменной типа int
+	int* ptr_a = &a; // указатель ptr_a "смотрит" на a
+	*c = 10;
+	cout << "*c = " << *c << endl;
+	cout << "*d = " << *d << endl;
+	a = 15;
+	cout << "*ptr_a = " << *ptr_a << endl; //выводится переменная a
+	cout << "ptr_a = " << ptr_a << endl; //выводится адрес переменной a
+	float *e = new float(3.14);
+	a = 3 + 5 + *c + b;
+	cout << "*ptr_a = " << *ptr_a << endl; //выводится переменная a
+	cout << "*float *e = " << *e - 0.01 << endl; //выводится резултат работы с переменной e типа float
+	delete e; // освобождает место
+//		cout << "*float *e = " << *e - 0.01 << endl; //вызывает останов программы?
+
+// динамические массивы	
+	int num; // Размер массива
+	num = 11;
+	int *p_darr = new int[num]; //выделение памяти для массива
+	for (int i = 0; i < num; i++) {
+		if (i == 0) p_darr[i] = 1;
+		else p_darr[i] = p_darr[i-1] + i ;
+		cout << "Значение " << i << " -го элемента равно " << p_darr[i] << endl;
+	}
+	system("pause");
+	return 0;
+}
