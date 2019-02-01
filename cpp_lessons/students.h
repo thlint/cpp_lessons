58a9249c89a1e513e63c017519401011a2f2b874
File404Found1 <File404Found1@yandex.ru>
2019-02-01 16:48:26 +0300

    classes 1

---
+++ cpp_lessons/students.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+class Students {
+public:
+	void set_name(std::string student_name) name = student_name; //установка имени студента
+	std::string get_name() return name; //получение имени студента
+	void set_last_name(std::string student_last_name) last_name = student_last_name; //установка фамилии студента
+	std::string get_last_name() return last_name; //получение фамилии студента
+	void set_scores(int student_scores[]) //установка промежуточных оценок
+	{
+		for (int i = 0; i < 5; i++) scores[i] = student_scores[i];
+	}
+	void set_average_ball(float ball) average_ball = ball; // установка среднего балла
+	float get_average_ball() return average_ball; // получение среднего балла
+private:
+	int scores[5]; // промежуточные оценки
+	float average_ball; //средний балл
+	std::string name;
+	std::string last_name
+	};
